5e06212dcc503d077be8b8f70a0e57e68019115c
Damien Delay <damo@Damiens-Mac-mini.local>
2014-09-04 00:28:21 +0100

    Initial Commit

---
+++ Travel/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  Travel
+//
+//  Created by Damien Delay on 04/09/2014.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
---
+++ TravelTests/TravelTests.m
@@ -0,0 +1,34 @@
+//
+//  TravelTests.m
+//  TravelTests
+//
+//  Created by Damien Delay on 04/09/2014.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TravelTests : XCTestCase
+
+@end
+
+@implementation TravelTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
